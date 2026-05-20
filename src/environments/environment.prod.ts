ad671c5d1a8d0a369516673bd6ca7581ed6a8d7c
Aeron Joshua Arañez <jakeaxel911@gmail.com>
2026-05-20 05:00:52 +0000

    Point frontend API to Railway backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://aranezfinalbackend-production.up.railway.app'
+  production: true,
+  apiUrl: 'https://aranezfinalbackend-production.up.railway.app'
 };
